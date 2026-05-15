094b24cc56079435df7392f3e1a5e5fa44b0d4df
Vladimir Orlyanskiy <vorlyanskiy@ibs.ru>
2026-05-15 23:05:40 +0300

    Приведеие к виду скрипта

--- Test1-data-source.sql
+++ Test1-data-source.sql
@@ -1,8 +1,7 @@
 SELECT
-    rt.file_type, cl.time_frame_start, cl.time_frame_finish, cl."load"
+    rt.file_type, cl."load"
 FROM
     cpu_load cl
 JOIN received_types rt ON
     rt.received_at >= cl.time_frame_start
-    AND rt.received_at < cl.time_frame_finish
-ORDER BY 2;
+    AND rt.received_at < cl.time_frame_finish;
